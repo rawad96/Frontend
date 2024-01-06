2062ed0de0ea08e2e94c0773e3f5ea773c0f2195
rawad96 <rawad3001@gmail.com>
2024-01-06 13:44:18 +0200

    initial commit

---
+++ myapp/src/Cinema.jsx
@@ -0,0 +1,15 @@
+import { Container, Card } from 'react-bootstrap'
+
+const Cinema = () => {
+    return (
+        <>
+            <Container style={{ position: "absolute", left: "25%", width: "50%", top: "50%" }}>
+                <Card>
+
+                </Card>
+            </Container>
+        </>
+    )
+}
+
+export default Cinema
